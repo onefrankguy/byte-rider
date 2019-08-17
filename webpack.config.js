0b53a4d922295baca333f0d0b641933907d30d6d
Frank Mitchell <me@frankmitchell.org>
2019-08-17 14:46:57 -0400

    Configure webpack to minify and inline JavaScript & HTML.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
-module.exports = {
-  devServer: {
-    stats: 'minimal',
-    overlay: true,
-  }
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  const config = {
+    entry: './src',
+    devtool: !isProduction && 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        minify: isProduction && {
+          collapseWhitespace: true,
+        },
+        inlineSource: isProduction && '\.js$',
+      }),
+      new HtmlWebpackInlineSourcePlugin(),
+    ],
+    devServer: {
+      stats: 'minimal',
+      overlay: true,
+    }
+  };
+
+  return config;
 };
