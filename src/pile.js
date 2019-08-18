ad346d4429a5aff5d50f38bef7a539c574842314
Frank Mitchell <me@frankmitchell.org>
2019-08-18 07:02:48 -0400

    Remove cards from piles.

--- src/pile.js
+++ src/pile.js
@@ -1,5 +1,13 @@
 const Pile = {};
 
+const DELIMITER = ':';
+
 Pile.includes = (pile, card) => pile.find((c) => c.indexOf(card) > -1) !== undefined;
+
+Pile.remove = (pile, card) => pile.reduce((acc, s) => {
+  const stack = s.split(DELIMITER).filter((c) => c !== card).join(DELIMITER);
+
+  return stack ? acc.concat(stack) : acc;
+}, []);
 
 module.exports = Pile;
--- test/pile.js
+++ test/pile.js
@@ -13,3 +13,11 @@ test('Pile#includes returns true when the card is stacked in the pile', () => {
 test('Pile#includes returns false when the card is not in the pile', () => {
   expect(Pile.includes(['AC', 'AD', 'AH'], 'AS')).toBe(false);
 });
+
+test('Pile#remove removes a card from the pile', () => {
+  expect(Pile.remove(['AC', 'AD', 'AS', 'AH'], 'AD')).toStrictEqual(['AC', 'AS', 'AH']);
+});
+
+test('Pile#remove removes a stacked card from the pile', () => {
+  expect(Pile.remove(['AC', 'AD:AS:AH'], 'AS')).toStrictEqual(['AC', 'AD:AH']);
+});
