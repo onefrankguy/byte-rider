e3a77da78dde7e310231bf419d7a52b113725bc1
Frank Mitchell <me@frankmitchell.org>
2019-08-25 11:28:04 -0400

    Remove unused rules.

--- src/rules2.js
+++
@@ -1,9 +0,0 @@
-const Rules = {};
-
-const ROYALS = ['8', 'J', 'Q', 'K'];
-const NUMBERS = ['A', '2', '3', '4', '5', '6', '7', '9', 'T'];
-
-Rules.isRoyal = (value) => !!ROYALS.find((v) => (value || '').startsWith(v));
-Rules.isNumber = (value) => !!NUMBERS.find((v) => (value || '').startsWith(v));
-
-module.exports = Rules;
