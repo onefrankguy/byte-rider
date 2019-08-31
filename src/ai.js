80b037e335ce09c52249a5b97509a05fd9df602d
Frank Mitchell <me@frankmitchell.org>
2019-08-31 06:52:47 -0400

    Give the AI the ability to block the player.

--- src/ai.js
+++ src/ai.js
@@ -1,4 +1,5 @@
 const Rules = require('./rules');
+const Table = require('./table');
 
 const AI = {};
 
@@ -12,10 +13,35 @@ AI.winning = (table, player) => {
   });
 };
 
+AI.blocking = (table, player) => {
+  const opponent = Table.opponent(player);
+  const score = Rules.score(table, opponent);
+  const moves = Rules.moves(table, player);
+
+  return moves.filter((move) => {
+    const [start] = move.split('-');
+    const chain = Rules.chain(table, player, start).filter((c) => c[0] === move);
+
+    return !!chain.find((c) => {
+      const test = Rules.play(table, player, c);
+      return Rules.score(test, opponent) < score;
+    });
+  });
+};
+
 AI.moves = (table, player) => {
   const winning = AI.winning(table, player);
   if (winning.length > 0) {
     return winning;
+  }
+
+  const opponent = Table.opponent(player);
+  const score = Rules.score(table, opponent);
+  if (score >= 11) {
+    const blocking = AI.blocking(table, player);
+    if (blocking.length > 0) {
+      return blocking;
+    }
   }
 
   return Rules.moves(table, player);
--- test/ai.js
+++ test/ai.js
@@ -13,3 +13,63 @@ test('AI#winning shows winning moves', () => {
 
   expect(moves).toEqual(['KH-Px']);
 });
+
+test('AI#blocking shows blocking discard(number) moves', () => {
+  const table = Table.create();
+  // X can discard a 2 or 4 to block
+  table.x.hand = ['2C', '3C', '4C', '8C'];
+  table.y.played = ['5H'];
+
+  const moves = AI.blocking(table, 'x');
+
+  expect(moves).toStrictEqual([
+    '2C-Dx',
+    '4C-Dx',
+  ]);
+});
+
+test('AI#blocking shows blocking discard(royal) moves', () => {
+  const table = Table.create();
+  // X can discard an A, 3, or 4 to block
+  table.x.hand = ['AC', '3C', '4C', '8C'];
+  table.y.played = ['KH'];
+
+  const moves = AI.blocking(table, 'x');
+
+  expect(moves).toStrictEqual([
+    'AC-Dx',
+    '3C-Dx',
+    '4C-Dx',
+  ]);
+});
+
+test('AI#blocking shows blocking transfer moves', () => {
+  const table = Table.create();
+  // X can discard an A, 3, or 4 to block
+  table.x.hand = ['AC', '3C', '4C', '8C'];
+  table.y.played = ['7H'];
+  table.jacked = { '7H': ['JH'] };
+
+  const moves = AI.blocking(table, 'x');
+
+  expect(moves).toStrictEqual([
+    'AC-Dx',
+    '3C-Dx',
+    '4C-Dx',
+  ]);
+});
+
+test('AI#blocking shows blocking scuttle moves', () => {
+  const table = Table.create();
+  // X can scuttle 3 or 4 to block
+  table.x.hand = ['AC', '3C', '4C', '8C'];
+  table.y.played = ['2H'];
+
+  const moves = AI.blocking(table, 'x');
+
+  expect(moves).toStrictEqual([
+    '3C-2H',
+    '4C-Dx',
+    '4C-2H',
+  ]);
+});
